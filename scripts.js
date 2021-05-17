ea4a043c5a402e5eb456446a0eea7357ad15b80c
kjin <kjin@mit.edu>
2021-05-17 00:18:31 -0400

    states and static badge1

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
 $(".navigation tr").click(function(){
-    $(this).addClass('selected').siblings().removeClass('selected');    
+    $(this).addClass('selected').siblings().removeClass('selected');   
+    displayGym(this.rowIndex) 
  });
  
- $('.ok').on('click', function(e){
-     alert($("#table tr.selected td:first").html());
- });
+//  $('.ok').on('click', function(e){
+//      alert($("#table tr.selected td:first").html());
+//  });
+
+
+ function displayGym(idx) {
+    // $(".overlay-route").prepend('<img src="gamepath1.png" />')
+    const i = String(idx)
+    $(".route").css("display", "none");
+    $(".route-"+i).css("display", "inline");
+ }
